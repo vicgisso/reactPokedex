3c9422b5934089e05998af3b96445c73619e7f8c
Unknown <unknown@example.com>
2017-11-04 05:17:20 +0100

    [Bootstrap & CSS] improved behaviour in rows

--- app/pokemon/pokemon_table_row.component.js
+++ app/pokemon/pokemon_table_row.component.js
@@ -3,19 +3,23 @@ window.PokemonRow = React.createClass({
     render: function () {
         return (
             <tr>
-                <td>{this.props.pokemon.name}</td>
-                <td>{this.props.pokemon.description}</td>
+                <td className='center-justified text_wallAway' >
+                {this.props.pokemon.name}
+                </td>
+                <td className='center-justified text_wallAway' >
+                {this.props.pokemon.description}
+                </td>
                 <td><a href='#'
                         onClick={() => this.props.changeAppMode('readOne', this.props.pokemon.id)}
-                        className='btn btn-info m-r-1em'> Details
+                        className='btn btn-info text_wallAway'> Details
                 </a>
                 <a href='#'
                         onClick={() => this.props.changeAppMode('update', this.props.pokemon.id)}
-                        className='btn btn-primary m-r-1em'> Edit
+                        className='btn btn-primary text_wallAway'> Edit
                 </a>
                 <a
                         onClick={() => this.props.changeAppMode('delete', this.props.pokemon.id)}
-                        className='btn btn-danger'> Delete
+                        className='btn btn-danger text_wallAway'> Delete
                 </a>
                 </td>
             </tr>
